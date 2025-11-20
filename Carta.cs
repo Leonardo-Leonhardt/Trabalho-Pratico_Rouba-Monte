6464bf65c7d7bbc934976e8ee94c57f34f84e2d9
Leonardo Bispo <1453450@sga.pucminas.br>
2025-11-20 17:37:50 -0300

    Adicionar classe Carta com validação e representação
    Introduzida a classe `Carta` no namespace `Rouba_Monte`, que representa uma carta de baralho com atributos `_numero` e `_naipe`. O construtor valida os valores de naipe e número, lançando uma `ArgumentException` para valores inválidos. Métodos privados `ValidarNaipe` e `ValidarNumero` foram adicionados para verificar a validade dos atributos. O método `ToString` foi sobrescrito para fornecer uma representação em string da carta.

---
+++ Carta.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+
+namespace Rouba_Monte
+{
+    public class Carta
+    {
+        private int _numero;
+        private string _naipe;
+
+        public Carta(int numero, string naipe)
+        {
+            if (!ValidarNaipe(naipe))
+            {
+                throw new ArgumentException($"O naipe '{naipe}' é inválido. Naipes válidos são: copas, espadas, ouros ou paus.");
+            }
+
+            if (!ValidarNumero(numero))
+            {
+                throw new ArgumentException($"O valor '{numero}' para a carta é inválido. O número deve ser entre 1 (Ás) e 13 (Rei).");
+            }
+
+            this._numero = numero;
+            this._naipe = naipe;
+        }
+
+        private bool ValidarNaipe(string naipe)
+        {
+            if (naipe == "copas" || naipe == "espadas" || naipe == "ouros" || naipe == "paus")
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool ValidarNumero(int numero)
+        {
+            if (numero < 1 || numero > 13)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Carta");
+            sb.AppendLine($"Numero: {_numero}");
+            sb.AppendLine($"Naipe: {_naipe}");
+
+            return sb.ToString();
+        }
+    }
+}
